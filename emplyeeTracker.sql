29ae2d949fef3aaa42b32ea74035bec335de705e
Ashley Lerma <lerma.scout@gmail.com>
2020-07-10 11:05:38 -0500

    tables set up

--- emplyeeTracker.sql
+++ emplyeeTracker.sql
@@ -5,10 +5,20 @@ USE employee_DB;
 
 CREATE TABLE department(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL,
+  name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
+  salary DECIMAL (8,2) NOT NULL,
+  department_id INT NOT NULL
+);
+
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR (30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NULL
 );
