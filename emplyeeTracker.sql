0ceeb1fac4e7588b2ea844c285b041a738a3cb1f
juanlajara <juanlajara001@gmail.com>
2020-07-11 17:23:34 -0500

    + foreign Keys

--- emplyeeTracker.sql
+++ emplyeeTracker.sql
@@ -12,13 +12,19 @@ CREATE TABLE role(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL (8,2) NOT NULL,
-  department_id INT NOT NULL
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id)
+     REFERENCES department(id)
+     ON DELETE CASCADE
 );
 
 CREATE TABLE employee(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR (30) NOT NULL,
+  manager_id INT NULL,
   role_id INT NOT NULL,
-  manager_id INT NULL
+  FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE CASCADE
 );
